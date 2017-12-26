19b541a2549f8c5b93b99a09751216116c694861
Михаил Путилов <post.snowy@gmail.com>
2017-12-26 22:22:26 +0300

    new versionʼ

--- src/main/scripts/release_pom.groovy
+++ src/main/scripts/release_pom.groovy
@@ -5,11 +5,32 @@ def releaseBranchName = "release/$releaseVersion"
 def checkpointTagName = "$releaseVersion-checkpoint"
 def developBranchName = 'develop'
 
-enum ExitStatus {
-    SUCCESS, FAILFAST, READY
 
-    static def getStatusByExitCode(int exitStatus) {
-        exitStatus == 0 ? SUCCESS : FAILFAST
+def project = new XmlSlurper().parse(new File("pom.xml"));
+String currentDir = new File(".").getAbsolutePath()
+
+/**
+ * Run commands in all modules
+ */
+def run(commands) {
+    // module dir -> executed commands
+    executionLog = [:]
+    project.modules.children().each {
+        try {
+            executionLog["$it"] = []
+            def dir = Paths.get(currentDir, "$it")
+            commands.each {
+                try {
+                    cmd.executeCommand()
+                } finally {
+                    // always put commands even if command failed
+                    executionLog["$it"].add(cmd)
+                }
+            }
+        } catch (RuntimeException e) {
+            e.executionLog = executionLog
+            throw e;
+        }
     }
 }
 
@@ -21,26 +42,23 @@ class Command {
         println command
         def process = command.execute()
         process.waitFor()
-        def exitStatus = ExitStatus.getStatusByExitCode(process.exitValue())
-        if (exitStatus == ExitStatus.FAILFAST) {
+
+        if (process.exitValue()) {
             print "${process.err.text}\n"
             throw new RuntimeException("$command.failExplanation")
         }
         print process.in.text
-        exitStatus
     }
 }
 
 class GitCommand extends Command {
-    String dir
+    def dir
+
     GitCommand(dir, cmd, failExplanation) {
-        super(cmdPrefix: "git -C $dir", cmd: cmd, failExplanation: failExplanation)
+        super(cmd: "git -C $dir $cmd", failExplanation: failExplanation)
+        this.dir = dir
     }
 }
-
-def exitStatus = ExitStatus.READY
-def project = new XmlSlurper().parse(new File("pom.xml"));
-def currentDir = new File(".").getAbsolutePath()
 
 def validateCommands = [
         new GitCommand(cmd: 'status --porcelain',
@@ -48,35 +66,47 @@ def validateCommands = [
         new GitCommand(cmd: "checkout $developBranchName",
                 failExplanation: "cannot checkout $developBranchName branch"),
         new GitCommand(cmd: "pull origin $developBranchName",
-                failExplanation: "cannot pull new changes for $developBranchName branch"),
-        new GitCommand(cmd: "tag $checkpointTagName",
-                failExplanation: "cannot create checkpoint (git tag $checkpointTagName). Probably repository is in inconsistent state with the others"),
+                failExplanation: "cannot pull new changes for $developBranchName branch")
 ]
+def checkpointCommand = new GitCommand(cmd: "tag $checkpointTagName",
+        failExplanation: "cannot create checkpoint (git tag $checkpointTagName). Probably repository is in inconsistent state with the others")
 
-def checkpointCommand = new Command(cmd: "checkout -b $releaseBranchName",
-        failExplanation: "cannot create release branch: $releaseBranchName")
-def setReleaseVersionsByMvn = new Command(cmd: """mvn versions:set -DgenerateBackupPoms=false
-    -DprocessAllModules=true
-    -DnewVersion="1.0.0"
-    -DoldVersion=*
-    -DgroupId=*
-    -DartifactId=*""", failExplanation: 'Maven cannot update version of all modules')
-
-
-
-
-project.modules.children().each {
-    def dir = Paths.get(currentDir, "$it")
-    def git = "git -C $dir"
-    validateCommands.each {
-        executeCommand("$git $it")
-    }
+///////////////////////////////////////////////////
+try {
+    run(validateCommands)
+} catch (RuntimeException e) {
+    println 'Cannot validate project. No changes have been made.'
+    println e.message
+    println 'Execution log:'
+    println e.executionLog
 }
-
-checkpointCommand.executeCommand()
-setReleaseVersionsByMvn.executeCommand()
-
-
-new Command(cmd: "checkout -b $releaseBranchName", failExplanation: "cannot create release branch: $releaseBranchName")
-
-return exitCode
+try {
+    run([checkpointCommand.executeCommand()])
+} catch (RuntimeException e) {
+    println 'Cannot create checkpoint.'
+    println e.message
+    println 'Execution log:'
+    println e.executionLog
+}
+//
+//def createReleaseBranchCommand = new Command(cmd: "checkout -b $releaseBranchName",
+//        failExplanation: "cannot create release branch: $releaseBranchName")
+//def setReleaseVersionsByMvnCommand = new Command(cmd: """mvn versions:set -DgenerateBackupPoms=false
+//    -DprocessAllModules=true
+//    -DnewVersion="${releaseVersion}"
+//    -DoldVersion=*
+//    -DgroupId=*
+//    -DartifactId=*""", failExplanation: 'Maven cannot update version of all modules')
+//
+//checkpointCommand.executeCommand()
+//currentState = ExitStatus.SUCCESS // now we have something to lose, because we created checkpoint
+//
+//setReleaseVersionsByMvnCommand.executeCommand()
+//
+//
+//new Command(cmd: "checkout -b $releaseBranchName", failExplanation: "cannot create release branch: $releaseBranchName")
+//
+//checkpointCommand
+//        .success(createReleaseBranchCommand)
+//        .error()
+//return exitCode
