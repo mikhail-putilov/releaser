27e46c710bf3d95514e0e0dc702f69347dd36914
Михаил Путилов <post.snowy@gmail.com>
2017-12-26 23:37:59 +0300

    new versionʼ

--- src/main/scripts/release_pom.groovy
+++ src/main/scripts/release_pom.groovy
@@ -7,28 +7,54 @@ def developBranchName = 'develop'
 
 
 def project = new XmlSlurper().parse(new File("pom.xml"));
-String currentDir = new File(".").getAbsolutePath()
-
+currentDir = new File(".").getAbsolutePath()
+profile = project.profiles.children().find { profile ->
+    profile.id == 'sub-modules'
+}
+class Phase {
+    String phaseTitle
+    String phaseFailAdditional
+    List<Command> cmds
+    Phase(phaseTitle, phaseFailAdditional, cmds) {
+        this.phaseTitle = phaseTitle
+        this.phaseFailAdditional = phaseFailAdditional
+        this.cmds = cmds
+    }
+    def runPhase() {
+        try {
+            println "***** $phaseTitle *****"
+            run(cmds)
+        } catch (RuntimeException e) {
+            System.err.println "Exception during $phaseTitle. $phaseFailAdditional\nException message:"
+            System.err.println e.message
+            e.printStackTrace()
+            System.err.println "Execution log of the phase ${phaseTitle.toLowerCase}:"
+            System.err.println e.executionLog
+        }
+    }
+}
 /**
  * Run commands in all modules
  */
 def run(commands) {
     // module dir -> executed commands
     executionLog = [:]
-    project.modules.children().each {
+    profile.modules.children().each {
         try {
-            executionLog["$it"] = []
+            String currentModule = "$it".toString()
+            executionLog[currentModule] = []
             def dir = Paths.get(currentDir, "$it")
             commands.each {
                 try {
-                    cmd.executeCommand()
+                    it.prepare(dir)
+                    it.executeCommand()
                 } finally {
                     // always put commands even if command failed
-                    executionLog["$it"].add(cmd)
+                    executionLog[currentModule].add(it)
                 }
             }
         } catch (RuntimeException e) {
-            e.executionLog = executionLog
+            e.metaClass.executionLog = executionLog
             throw e;
         }
     }
@@ -38,55 +64,82 @@ class Command {
     String cmd
     String failExplanation
 
+    Command(cmd, failExplanation) {
+        this.cmd = cmd
+        this.failExplanation = failExplanation
+    }
+
     def executeCommand() {
-        println command
-        def process = command.execute()
+        println cmd
+        def process = cmd.execute()
         process.waitFor()
 
         if (process.exitValue()) {
             print "${process.err.text}\n"
-            throw new RuntimeException("$command.failExplanation")
+            throw new RuntimeException(failExplanation)
         }
         print process.in.text
+    }
+
+    void prepare(dir) {
+        // noop
+    }
+
+    String toString() {
+        return cmd
     }
 }
 
 class GitCommand extends Command {
     def dir
+    def subcmd
 
-    GitCommand(dir, cmd, failExplanation) {
-        super(cmd: "git -C $dir $cmd", failExplanation: failExplanation)
+    GitCommand(String subcmd, String failExplanation) {
+        super('', failExplanation)
+        this.subcmd = subcmd
+    }
+
+    void prepare(dir) {
         this.dir = dir
+        this.cmd = "git -C $dir $subcmd"
+    }
+
+    String toString() {
+        return "git $subcmd"
     }
 }
 
 def validateCommands = [
-        new GitCommand(cmd: 'status --porcelain',
-                failExplanation: 'working dir is not clean'),
-        new GitCommand(cmd: "checkout $developBranchName",
-                failExplanation: "cannot checkout $developBranchName branch"),
-        new GitCommand(cmd: "pull origin $developBranchName",
-                failExplanation: "cannot pull new changes for $developBranchName branch")
+        new GitCommand('status --porcelain',
+                'working dir is not clean'),
+        new GitCommand("checkout $developBranchName",
+                "cannot checkout $developBranchName branch"),
+        new GitCommand("pull origin $developBranchName",
+                "cannot pull new changes for $developBranchName branch")
 ]
-def checkpointCommand = new GitCommand(cmd: "tag $checkpointTagName",
-        failExplanation: "cannot create checkpoint (git tag $checkpointTagName). Probably repository is in inconsistent state with the others")
+
+validatePhase = new Phase("Validation", "No chanes have been made", validateCommands)
+def checkpointCommand = new GitCommand("tag $checkpointTagName",
+        "cannot create checkpoint (git tag $checkpointTagName). Probably repository is in inconsistent state with the others")
 
 ///////////////////////////////////////////////////
 try {
     run(validateCommands)
 } catch (RuntimeException e) {
-    println 'Cannot validate project. No changes have been made.'
-    println e.message
-    println 'Execution log:'
-    println e.executionLog
+    System.err.println 'Cannot validate project. No changes have been made. Exception message:'
+    System.err.println e.message
+    e.printStackTrace()
+    System.err.println 'Execution log:'
+    System.err.println e.executionLog
 }
 try {
-    run([checkpointCommand.executeCommand()])
+    run([checkpointCommand])
 } catch (RuntimeException e) {
-    println 'Cannot create checkpoint.'
-    println e.message
-    println 'Execution log:'
-    println e.executionLog
+    System.err.println 'Cannot create checkpoint.'
+    System.err.println e.message
+    e.printStackTrace()
+    System.err.println 'Execution of the log:'
+    System.err.println e.executionLog
 }
 //
 //def createReleaseBranchCommand = new Command(cmd: "checkout -b $releaseBranchName",
